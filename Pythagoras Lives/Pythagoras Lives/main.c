8ba797743b202c17ccceb2d0809dfc889c8068ec
Kaitlyn Huynh <kaitlyn.huynh11@gmail.com>
2019-09-13 09:12:22 -0700

    pythagoras lives
    kh

---
+++ Pythagoras Lives/Pythagoras Lives/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Pythagoras Lives
+//
+//  Created by Huynh, Kaitlyn on 9/13/19.
+//  Copyright © 2019 Huynh, Kaitlyn. All rights reserved.
+//
+
+#include <stdio.h>
+#include "pythagoras.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    pythagoras();
+    return 0;
+}
