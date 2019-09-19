512fd85db115af7dcce63f88b825d6c81114c6ce
Kaitlyn Huynh <kaitlyn.huynh11@gmail.com>
2019-09-19 09:13:07 -0700

    update

--- Portfolio/main.c
+++ Portfolio/main.c
@@ -10,6 +10,17 @@
 #include "rectangle.h"
 
 int main(int argc, const char * argv[]) {
+    printf("=====================================\n");
+    printf("|   MENU SELECTION PORTFOLIO\n");
+    printf("=====================================\n");
+    printf("| Options:\n");
+    printf("|        1. Run pythagoras\n");
+    printf("|        2. Run rectangle\n");
+    printf("|        3. Run time operator\n");
+    printf("|        4. Run temperature\n");
+    printf("|        5. Run data types\n");
+    printf("|        6. Exit\n");
+    printf("=====================================\n");
     pythagoras();
     rectangle();
     operator();
