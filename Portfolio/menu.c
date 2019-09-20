4729389ebdda0aeaf51594a2b19b49be9bba6e4b
Kaitlyn Huynh <kaitlyn.huynh11@gmail.com>
2019-09-20 08:42:55 -0700

    kh

---
+++ Portfolio/menu.c
@@ -0,0 +1,20 @@
+//
+//  menu.c
+//  Portfolio
+//
+//  Created by Huynh, Kaitlyn on 9/20/19.
+//  Copyright © 2019 Huynh, Kaitlyn. All rights reserved.
+//
+
+#include "menu.h"
+void menu(void) {
+    printf("=====================================\n");
+    printf("|   MENU SELECTION PORTFOLIO\n");
+    printf("=====================================\n");
+    printf("| Options:\n");
+    printf("|        1. Run pythagoras\n");
+    printf("|        2. Run rectangle\n");
+    printf("|        3. Run time operator\n");
+    printf("|        4. Exit\n");
+    printf("=====================================\n");
+}
